c9f11c34ddb3c40c7adc20db3dbb216b77e1828e
borko81 <bstoilov81@gmail.com>
2022-06-28 18:33:04 +0300

    Add min date in input date fields

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous">
+    <script src="js/jquery-ui.js"></script>
     </script>
   <script src=check_for_free.js></script>
     <title>Reservations form</title>
@@ -40,6 +41,7 @@
         <div class="form-group">
             <label for="income_date">Дата на пристигане</label>
             <input type="date" class="form-control" id="income_date" name='income_date'>
+            
         </div>
 
         <div class="form-group">
@@ -138,6 +140,29 @@
 <!--Modal ends here--->
 
 <script type="text/javascript">
+
+    var date = new Date();
+    var today = new Date();
+    var tommorow = new Date();
+
+    var dd = today.getDate();
+    var tommorow_day = dd + 1;
+    var mm = today.getMonth()+1;
+    var yyyy = today.getFullYear();
+    if(dd<10){
+    dd='0'+dd
+    } 
+    if(mm<10){
+    mm='0'+mm
+    } 
+
+    today = yyyy+'-'+mm+'-'+dd;
+    tommorow = yyyy+'-'+mm+'-'+tommorow_day;
+
+    document.getElementById('income_date').valueAsDate = date;
+    document.getElementById("income_date").setAttribute("min", today);
+
+    document.getElementById("outcome_date").setAttribute("min", tommorow);
 
     $('form').submit(function() {
         let income = document.getElementById("income_date").value
